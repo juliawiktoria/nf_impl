a8c2f5d36cfcd72639a51fa4a9005b829f2a4c6c
juliawiktoria <maybe.nott123@gmail.com>
2021-05-13 11:51:15 +0100

    asdasd

--- layers.py
+++ layers.py
@@ -145,50 +145,61 @@ class Squeeze(nn.Module):
 
 class Invertible1x1ConvLU(nn.Module):
     # https://github.com/y0ast/Glow-PyTorch/blob/master/modules.py
-    def __init__(self, num_features):
+    def __init__(self, num_features, LU_decomposed=True):
         super(Invertible1x1ConvLU, self).__init__()
         w_shape = [num_features, num_features]
         w_init = torch.qr(torch.randn(*w_shape))[0]
 
-        p, lower, upper = torch.lu_unpack(*torch.lu(w_init))
-        s = torch.diag(upper)
-        sign_s = torch.sign(s)
-        log_s = torch.log(torch.abs(s))
-        upper = torch.triu(upper, 1)
-        l_mask = torch.tril(torch.ones(w_shape), -1)
-        eye = torch.eye(*w_shape)
+        if not LU_decomposed:
+            self.weight = nn.Parameter(torch.Tensor(w_init))
+        else:
+            p, lower, upper = torch.lu_unpack(*torch.lu(w_init))
+            s = torch.diag(upper)
+            sign_s = torch.sign(s)
+            log_s = torch.log(torch.abs(s))
+            upper = torch.triu(upper, 1)
+            l_mask = torch.tril(torch.ones(w_shape), -1)
+            eye = torch.eye(*w_shape)
 
-        self.register_buffer("p", p)
-        self.register_buffer("sign_s", sign_s)
-        self.lower = nn.Parameter(lower)
-        self.log_s = nn.Parameter(log_s)
-        self.upper = nn.Parameter(upper)
-        self.l_mask = l_mask
-        self.eye = eye
-
+            self.register_buffer("p", p)
+            self.register_buffer("sign_s", sign_s)
+            self.lower = nn.Parameter(lower)
+            self.log_s = nn.Parameter(log_s)
+            self.upper = nn.Parameter(upper)
+            self.l_mask = l_mask
+            self.eye = eye
+        
+        self.if_LU = LU_decomposed
         self.w_shape = w_shape
 
     def get_weight(self, input, reverse):
         b, c, h, w = input.shape
 
-        self.l_mask = self.l_mask.to(input.device)
-        self.eye = self.eye.to(input.device)
-
-        lower = self.lower * self.l_mask + self.eye
-
-        u = self.upper * self.l_mask.transpose(0, 1).contiguous()
-        u += torch.diag(self.sign_s * torch.exp(self.log_s))
-
-        dlogdet = torch.sum(self.log_s) * h * w
-
-        if reverse:
-            u_inv = torch.inverse(u)
-            l_inv = torch.inverse(lower)
-            p_inv = torch.inverse(self.p)
-
-            weight = torch.matmul(u_inv, torch.matmul(l_inv, p_inv))
+        if not self.if_LU:
+            dlogdet = torch.slogdet(self.weight)[1] * h * w
+            if not reverse:
+                weight = self.weight
+            else:
+                weight = torch.inverse(self.weight)
         else:
-            weight = torch.matmul(self.p, torch.matmul(lower, u))
+            self.l_mask = self.l_mask.to(input.device)
+            self.eye = self.eye.to(input.device)
+
+            lower = self.lower * self.l_mask + self.eye
+
+            u = self.upper * self.l_mask.transpose(0, 1).contiguous()
+            u += torch.diag(self.sign_s * torch.exp(self.log_s))
+
+            dlogdet = torch.sum(self.log_s) * h * w
+
+            if not reverse:
+                weight = torch.matmul(self.p, torch.matmul(lower, u))
+            else:
+                u_inv = torch.inverse(u)
+                l_inv = torch.inverse(lower)
+                p_inv = torch.inverse(self.p)
+
+                weight = torch.matmul(u_inv, torch.matmul(l_inv, p_inv))
 
         return weight.view(self.w_shape[0], self.w_shape[1], 1, 1), dlogdet
 
